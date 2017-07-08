87371f3690072e472e7c0896edf8eb4efcc7cb6c
jairo <jairochaves2@gmail.com>
2017-07-08 18:36:32 -0300

    navbar

---
+++ bootstrap-3.3.7-dist/copia.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+#Copia o diretorio em que estou desenvolvendo, para a pasta do lamp
+while true
+do
+sudo cp -r ~/Documentos/Bootstrap/bootstrap-3.3.7-dist/ /var/www/html/Bootstrap/
+echo copiou
+sleep 13
+clear
+sleep 2
+done
